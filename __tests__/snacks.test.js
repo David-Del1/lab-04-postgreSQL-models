a5da3d9f925b986e26f42afde0094015da799b91
David Delgadillo <daviddel.731@gmail.com>
2021-06-14 11:59:35 -0700

    GET route for all snacks done

--- __tests__/snacks.test.js
+++ __tests__/snacks.test.js
@@ -2,6 +2,7 @@ import pool from '../lib/utils/pool.js';
 import setup from '../data/setup.js';
 import request from 'supertest';
 import app from '../lib/app.js';
+import Snack from '../lib/models/Snack.js';
 
 describe('Snack CRUD routes', () => {
   beforeEach(() => {
@@ -19,8 +20,30 @@ describe('Snack CRUD routes', () => {
       type: 'chips',
       flavor: 'cheese'
     });
+  });
 
+  it('Should read all snacks via GET', async () => {
 
+    const snack1 = await Snack.insert({
+      name: 'Carrots',
+      type: 'veggie',
+      flavor: 'beta cerotene'
+    });
+    const snack2 = await Snack.insert({
+      name: 'Jelly bean',
+      type: 'candy',
+      flavor: 'grass'
+    });
+    const snack3 = await Snack.insert({
+      name: 'Kettle corn',
+      type: 'popcorn',
+      flavor: 'caramel'
+    });
+
+    const res = await request(app)
+      .get('/api/v1/snacks');
+
+    expect(res.body).toEqual([snack1, snack2, snack3]);
   });
 
   
